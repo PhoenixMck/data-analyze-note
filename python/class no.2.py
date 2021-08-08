e0aea00b828a9d31c22c2c76de75056a0ef51328
Phoenix <Phoenix_MCK@163.com>
2021-08-09 02:05:12 +0800

    补充字典相关信息

--- python/class no.2.py
+++ python/class no.2.py
@@ -8,6 +8,10 @@ print(a['Catherine'])  # 键是字符串，就要写引号,单双引号一样
 # 不可以有重复的键【key】，给原先的键赋值会覆盖,但是值可以重复
 a["Kate"] = "no.1"
 print(a)
+# 可以直接打印字典的键
+print(a.keys())
+#  get () 函数返回指定键的值，如果值不在字典中返回默认值[none]
+print(a.get("Kate"))
 # 可以通过赋值直接插入,但列表不可以这样写会报错
 a["phoenix"] = "no.4"
 print(a)
@@ -27,15 +31,30 @@ else:
     print("'nate' isn't in a,add it now")
     a["nate"] = 1
     print(a)
-b=[1,2]
-c=1
+b = [1, 2]
+c = 1
 if c in b:
     print(c in b)
-else: 
-    print("c isn't in b")  
+else:
+    print("c isn't in b")
 # 字典删除 del允许通过键删除一个独立元素，方法clear可以直接所有
 del a["Morphy"]
 print(a)
-a.pop("Kate")  # 效果同del
+d = a.pop("Kate")  # 效果同del，必须带参数--键,字典使用pop用法同list
+print(d)
+print(a)
 a.clear()
 print(a)
+
+# 元组 tuple 两边用圆括号，不可修改的，其索引依旧为方括号
+# 元组相当于一个不可修改的list，一样是用数字索引，可负数可切片(切片同list切片一样，实际是返回一个新元组)
+t = (1, 2, 3)
+print(t, t[0:2])
+t.index()
+# tuple没有方法，无法修改。连index都没有，只能用in判断是否在元组内，tuple比list更快，所以不需要查看修改，只需要遍历的，用list比用tuple快
+# tuple、list转换
+l = list(t)
+print(type(l), l)
+# tuple可以作为字典的一个键，但是list不行，因为字典的键一旦确定无法更改
+dic = {t: 1}
+print(dic)
