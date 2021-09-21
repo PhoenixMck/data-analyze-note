43141c2d0054d7494d98495fa611abd61bbb7120
Phoenix <Phoenix_MCK@163.com>
2021-09-21 19:54:00 +0800

    补充画图形练习

--- python/小练习 画图形.py
+++ python/小练习 画图形.py
@@ -6,11 +6,27 @@ import time as t
 """
 
 for num in range(3, 13):
-    home()
     """画n边形"""
     per_angle = 360 / num
     for i in range(0, num):
         angle = per_angle * i
         setheading(angle)
         forward(50)
+
+reset()
+"""
+以该图形的最后一条边作为下一个图形的第一条边
+"""
+end_angle = 0
+for num in range(3, 12):
+    """画n边形"""
+    per_angle = 360 / num
+    start_angle = end_angle
+    for i in range(0, num):
+        angle = start_angle + per_angle * i
+        setheading(angle)
+        forward(50)
+    else:
+        end_angle = angle + 180
+
 t.sleep(2)
