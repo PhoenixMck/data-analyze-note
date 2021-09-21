dfa7cfd4d7d80838e85bfabb24e8383b3eb1942a
Phoenix <Phoenix_MCK@163.com>
2021-09-22 00:16:22 +0800

    补充

--- python/小练习 画图形.py
+++ python/小练习 画图形.py
@@ -17,13 +17,12 @@ reset()
 """
 以该图形的最后一条边作为下一个图形的第一条边
 """
-end_angle = 0
+end_angle = 0.0
 for num in range(3, 12):
     """画n边形"""
     per_angle = 360 / num
-    start_angle = end_angle
     for i in range(0, num):
-        angle = start_angle + per_angle * i
+        angle = end_angle + per_angle * i
         setheading(angle)
         forward(50)
     else:
